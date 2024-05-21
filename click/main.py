e8c995f8873eab1d27a964c558b7d50b10648259
castromx <96194271+castromx@users.noreply.github.com>
2024-05-21 23:15:25 +0200

    add CORSmiddleware in main.py

--- click/main.py
+++ click/main.py
@@ -11,6 +11,20 @@ from database.database import get_db_session
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def root():
     return status.HTTP_200_OK
