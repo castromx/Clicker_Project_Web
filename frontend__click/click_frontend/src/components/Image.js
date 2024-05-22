a173ac60937639d2641946c77590301e63fd49b1
castromx <96194271+castromx@users.noreply.github.com>
2024-05-22 21:21:46 +0200

    Update Image.js
    add a onClick props

--- frontend__click/click_frontend/src/components/Image.js
+++ frontend__click/click_frontend/src/components/Image.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 class Image extends React.Component {
     render() {
         return (
-            <img src={this.props.image} />
-        )
+            <img src={this.props.image} alt="User Icon" onClick={this.props.onClick} />
+        );
     }
 }
 
-export default Image
+export default Image;
