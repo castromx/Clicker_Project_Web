11d61b7fc184d94d203a47f05bb9adf1ed83c14e
castromx <96194271+castromx@users.noreply.github.com>
2024-06-04 22:38:59 +0200

    clan functional beta

---
+++ click/database/crud.py
@@ -0,0 +1,165 @@
+from sqlalchemy.orm import Session, joinedload
+from datetime import datetime
+from . import schemas, models
+from typing import List
+
+
+# Функція для створення нового користувача
+def create_user(db: Session, user: schemas.UserAccount):
+    time_now = datetime.utcnow()
+    user_data = user.dict(exclude={"register_at", "last_login_at"})
+    db_user = models.User(**user_data, register_at=time_now, last_login_at=time_now)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+# Функція для отримання користувача за його ID
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+# Функція для створення початкових балів користувача
+def create_user_score(db: Session, user_id: int):
+    user = get_user(db, user_id)
+    user_id = user.id
+    scores = models.UserScore(user_id=user_id, score=0)
+    db.add(scores)
+    db.commit()
+    db.refresh(scores)
+    return scores
+
+
+# Функція для отримання балів користувача за його ID
+def get_user_scores(db: Session, user_id: int):
+    return db.query(models.UserScore).filter(models.UserScore.user_id == user_id).first()
+
+
+# Функція для додавання балів користувачу
+def add_point(db: Session, count: int, user_id: int):
+    user = get_user(db, user_id)
+    user_id = user.id
+    score_user = get_user_scores(db, user_id)
+    score_user.score += count
+    db.add(score_user)
+    db.commit()
+    db.refresh(score_user)
+    return score_user
+
+
+# Функція для створення підсилення для користувача
+def create_user_boost(db: Session, user_id: int):
+    user = get_user(db, user_id)
+    user_id = user.id
+    boosts = models.Boosts(user_id=user_id, fill_char_count=1, charge_count=1, mine_coint=1)
+    db.add(boosts)
+    db.commit()
+    db.refresh(boosts)
+    return boosts
+
+
+# Функція для отримання підсилення користувача за його ID
+def get_user_boosts(db: Session, user_id: int):
+    return db.query(models.Boosts).filter(models.Boosts.user_id == user_id).first()
+
+
+# Функція для зменшення заряду підсилення у користувача
+def dev_charge(db: Session, user_id: int):
+    boosts = get_user_boosts(db, user_id)
+    count_miner = boosts.mine_coint
+    boosts.charge_count -= count_miner
+    db.commit()
+    return boosts.charge_count
+
+
+# Функція для створення зображення
+def create_image(db: Session, image_data: bytes):
+    db_image = models.Image(data=image_data)
+    db.add(db_image)
+    db.commit()
+    db.refresh(db_image)
+    return db_image
+
+
+# Функція для створення клану
+def create_clan(db: Session, clan: schemas.ClanCreate):
+    db_clan = models.Clan(name=clan.name, img_id=clan.img_id)
+    db.add(db_clan)
+    db.commit()
+    db.refresh(db_clan)
+    return db_clan
+
+
+# Функція для отримання клану за його ID
+def get_clan(db: Session, clan_id: int):
+    return db.query(models.Clan).filter(models.Clan.id == clan_id).first()
+
+
+# Функція для створення початкових балів клану
+def create_clan_score(db: Session, clan_id: int):
+    clan = get_clan(db, clan_id)
+    clan_id = clan.id
+    scores = models.ClanScore(clan_id=clan_id, score=0)
+    db.add(scores)
+    db.commit()
+    db.refresh(scores)
+    return scores
+
+
+# Функція для отримання балів клану за його ID
+def get_clan_scores(db: Session, clan_id: int):
+    return db.query(models.ClanScore).filter(models.ClanScore.clan_id == clan_id).first()
+
+
+# Функція для додавання балів клану
+def add_clan_point(db: Session, clan_id: int):
+    clan = get_clan(db, clan_id)
+    clan_id = clan.id
+    score_clan = get_clan_scores(db, clan_id)
+    score_clan.score += 1
+    db.add(score_clan)
+    db.commit()
+    db.refresh(score_clan)
+    return score_clan
+
+
+# Функція для приєднання користувача до клану
+def enter_in_clan(db: Session, clan_id: int, user_id: int):
+    db_clan_enter = models.UsersClan(user_id=user_id, clan_id=clan_id)
+    db.add(db_clan_enter)
+    db.commit()
+    db.refresh(db_clan_enter)
+    return db_clan_enter
+
+
+# Функція для отримання клану користувача за його ID
+# def get_clans_for_user(db: Session, user_id: int):
+#     return db.query(models.Clan).join(models.UsersClan).filter(models.UsersClan.user_id == user_id).first()
+
+def get_clans_for_user(db: Session, user_id: int):
+    return db.query(models.Clan).join(models.UsersClan).filter(models.UsersClan.user_id == user_id).options(
+        joinedload(models.Clan.count_score)
+    ).first()
+
+
+# Функція для виходу користувача з клану
+def leave_from_clan(db: Session, user_id: int):
+    user_clan_record = db.query(models.UsersClan).filter(models.UsersClan.user_id == user_id).first()
+    if user_clan_record:
+        db.delete(user_clan_record)
+        db.commit()
+        return {"msg": "Користувач покинув клан"}
+    else:
+        return {"msg": "Користувач не був у клані"}
+
+
+# def get_all_clan(db: Session):
+#     return db.query(models.Clan).all()
+
+def get_clans(db: Session):
+    return db.query(models.Clan).join(models.UsersClan).options(joinedload(models.Clan.count_score)).all()
+
+
+def get_image(db: Session, image_id: int):
+    return db.query(models.Image).filter(models.Image.id == image_id).first()
---
+++ click/main.py
@@ -0,0 +1,149 @@
+import aioredis
+from fastapi import FastAPI, status, Depends, HTTPException, UploadFile, File
+from starlette.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+
+from database import schemas, models, crud
+from sqlalchemy.orm import Session
+from database.database import get_db_session
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return status.HTTP_200_OK
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/create_user")
+async def create_user(user: schemas.UserAccount, db: Session = Depends(get_db_session)) -> schemas.UserAccount:
+    user = crud.create_user(db=db, user=user)
+    crud.create_user_score(db, user.id)
+    crud.create_user_boost(db, user.id)
+    return user
+
+
+@app.get("/get_user")
+async def get_user(id: int, db: Session = Depends(get_db_session)) -> schemas.UserAccount:
+    return crud.get_user(db, id)
+
+
+@app.get("/get_user_score")
+async def get_user_score(user_id: int, db: Session = Depends(get_db_session)):
+    return crud.get_user_scores(db, user_id)
+
+
+@app.post("/create_user_scores")
+async def create_user_scores(user_id: int, db: Session = Depends(get_db_session)):
+    return crud.create_user_score(db, user_id)
+
+
+@app.post("/add_user_scores")
+async def add_user_scores(user_id: int, count: int, db: Session = Depends(get_db_session)):
+    boosts = crud.get_user_boosts(db, user_id)
+    charge = boosts.charge_count * 600
+    count = boosts.mine_coint * count
+    if charge > 1:
+        crud.add_point(db, count, user_id)
+        clan = crud.get_clans_for_user(db, user_id)
+        if clan:
+            clan_id = clan.id
+            crud.add_clan_point(db, clan_id)
+        crud.dev_charge(db, user_id)
+        charge_after = boosts.charge_count
+        count_after = crud.get_user_scores(db, user_id)
+        return {"charge": charge_after, "count": count_after.score}
+    elif charge < 1:
+        return "Energy charge too low"
+
+
+@app.post("/create_user_boosts")
+async def create_user_boosts(user_id: int, db: Session = Depends(get_db_session)):
+    return crud.create_user_boost(db, user_id)
+
+
+@app.get("/get_user_boosts")
+async def get_user_boosts(user_id: int, db: Session = Depends(get_db_session)):
+    boosts = crud.get_user_boosts(db, user_id)
+    return boosts
+
+
+@app.post("/clans/", response_model=schemas.Clan)
+async def create_clan(clan: schemas.ClanCreate, db: Session = Depends(get_db_session)):
+    db_image = db.query(models.Image).filter(models.Image.id == clan.img_id).first()
+    clan = crud.create_clan(db=db, clan=clan)
+    crud.create_clan_score(db, clan.id)
+    if not db_image:
+        raise HTTPException(status_code=422, detail="Image not found")
+    return clan
+
+
+
+@app.get("/clans/{clan_id}", response_model=schemas.Clan)
+async def read_clan(clan_id: int, db: Session = Depends(get_db_session)):
+    db_clan = crud.get_clan(db=db, clan_id=clan_id)
+    if db_clan is None:
+        raise HTTPException(status_code=404, detail="Clan not found")
+    return db_clan
+
+
+
+@app.post("/uploadfile/")
+async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db_session)):
+    file_data = file.file.read()
+    db_image = crud.create_image(db=db, image_data=file_data)
+    return {"image_id": db_image.id}
+
+
+@app.post("/enter_in_clan")
+async def enter_in_clan(user_id: int, clan_id: int, db: Session = Depends(get_db_session)):
+    if crud.get_clans_for_user(db, user_id):
+        return {"Вам потрібно вийти з попереднього клану, щоб приєднатись у цей"}
+    return crud.enter_in_clan(db, user_id, clan_id)
+
+
+@app.get("/get_user_clan")
+async def get_user_clan(user_id: int, db: Session = Depends(get_db_session)):
+    return crud.get_clans_for_user(db, user_id)
+
+
+@app.delete("/leave_from_clan")
+async def leave_from_clan(user_id: int, db: Session = Depends(get_db_session)):
+    return crud.leave_from_clan(db, user_id)
+
+
+@app.post("/add_point_clan")
+async def add_point_clan(clan_id: int, db: Session = Depends(get_db_session)):
+    return crud.add_clan_point(db, clan_id)
+
+@app.get('/get_all_clans')
+async def get_all_clans(db: Session = Depends(get_db_session)):
+    return crud.get_clans(db)
+
+
+@app.get("/images/{image_id}")
+async def read_image(image_id: int, db: Session = Depends(get_db_session)):
+    db_image = crud.get_image(db, image_id=image_id)
+    if db_image is None:
+        raise HTTPException(status_code=404, detail="Image not found")
+
+    temp_file_path = f"temp_image_{image_id}.png"
+    with open(temp_file_path, "wb") as temp_file:
+        temp_file.write(db_image.data)
+
+    return FileResponse(temp_file_path, media_type='image/png', filename=f"image_{image_id}.png")
