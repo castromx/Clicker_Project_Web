2f931380b5c716ec95585f2bcec17072657a3030
castromx <96194271+castromx@users.noreply.github.com>
2024-07-02 22:27:54 +0200

    Update tasks.py

--- click/tasks.py
+++ click/tasks.py
@@ -16,8 +16,8 @@ def add_point_task(user_id: int, count: int):
     return score
 
 @celery.task
-def add_charge_count(db: Session, user_id: int, user.boosts.charge):
+def add_charge_count(db: Session, user_id: int, user.boosts.point):
     bosts = crud.get_user_boosts(db, user_id)
-    bosts.charge_count += 1
+    bosts.charge_count += user.boosts.point
     db.commit()
     return bosts.charge_count
