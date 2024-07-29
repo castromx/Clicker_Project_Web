fb5ae8000081c76bd4305bf21bbf5c5d84d7819f
castromx <96194271+castromx@users.noreply.github.com>
2024-07-29 22:51:43 +0200

    Update Image.js

--- frontend__click/click_frontend/src/Pages/components/Image.js
+++ frontend__click/click_frontend/src/Pages/components/Image.js
@@ -1,15 +1,6 @@
 import React, { useState } from 'react';
 
-const Image = (props) => {
-    const [animate, setAnimate] = useState(false);
-
-    const handleClick = () => {
-        setAnimate(true);
-        setTimeout(() => setAnimate(false), 1000);
-        if (props.onClick) {
-            props.onClick();
-        }
-    };
+const Image{
 
     return (
         <img
