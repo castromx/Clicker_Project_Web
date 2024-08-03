4cd94b181178b0b4e64b3ba772a443c697273ff6
castromx <96194271+castromx@users.noreply.github.com>
2024-08-03 19:33:00 +0200

    upd env.py for async engine

--- click/database/database.py
+++ click/database/database.py
@@ -3,7 +3,7 @@ from typing import AsyncGenerator
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
-from ..config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
