03ff7cd4f974dc03210cf7a04a962038d14d23c0
castromx <96194271+castromx@users.noreply.github.com>
2024-08-11 22:39:02 +0200

    Update database.py

--- click/database/database.py
+++ click/database/database.py
@@ -1,13 +1,11 @@
 from typing import AsyncGenerator
-
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine, AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
-class Base(DeclarativeBase):
+class Base(DeclarativeBase, AsyncAttrs):
     pass
 
 
@@ -19,8 +17,6 @@ async def create_db_and_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-
-from sqlalchemy.ext.asyncio import AsyncSession
 
 async def get_async_session() -> AsyncSession:
     async with Session() as session:
