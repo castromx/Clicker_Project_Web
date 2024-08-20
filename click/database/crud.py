b16c2a93f003975342a74df6e0b2992c86920357
castromx <96194271+castromx@users.noreply.github.com>
2024-08-20 21:42:25 +0200

    Update crud.py

--- click/database/crud.py
+++ click/database/crud.py
@@ -11,13 +11,9 @@ async def create_user(db: AsyncSession, user: schemas.UserAccount):
     user_data = user.dict(exclude={"register_at", "last_login_at", "scores", "charges"})
     db_user = models.User(**user_data, register_at=time_now, last_login_at=time_now)
     db.add(db_user)
-    try:
-        await db.commit()
-    except Exception as e:
-        print('User Already Exists')
-    else:
-        await db.refresh(db_user)
-        return db_user
+    await db.commit()
+    await db.refresh(db_user)
+    return db_user
 
 
 # Функція для отримання користувача за його ID
