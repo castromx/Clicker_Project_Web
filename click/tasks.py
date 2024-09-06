cf37a1d3a85c3ffa5d70d99cb031a3dcc415cfd4
castromx <96194271+castromx@users.noreply.github.com>
2024-09-06 21:59:10 +0200

    Update tasks.py

--- click/tasks.py
+++ click/tasks.py
@@ -1,22 +1,21 @@
 from celery import Celery
-from sqlalchemy.ext.asyncio import AsyncSession
-from database import database, crud, schemas
-from database.database import DATABASE_URL, get_async_session
+from sqlalchemy.ext.asyncio import async_session
+from database import crud
 
 celery = Celery('tasks', broker='redis://localhost:6379')
 
 @celery.task
 async def add_point_task(user_id: int, count: int):
-    async with async_session() as session:  
+    async with async_session() as session:
         async with session.begin():
             score = await crud.add_point(session, count, user_id)
         return score
 
 @celery.task
 async def add_charge_count(user_id: int):
-    async with async_session() as session:  
+    async with async_session() as session:
         async with session.begin():
             boosts = await crud.get_user_boosts(session, user_id)
             boosts.charge_count += 1
-            await session.commit()  
+            await session.commit()
         return boosts.charge_count
