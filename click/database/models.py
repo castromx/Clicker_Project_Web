f980a5e5e9ee1903059c47502b9d60cade014ac3
castromx <96194271+castromx@users.noreply.github.com>
2024-09-11 20:32:18 +0200

    Update models.py

--- click/database/models.py
+++ click/database/models.py
@@ -1,10 +1,7 @@
 from datetime import datetime
-from sqlalchemy import ForeignKey, LargeBinary, Integer
-from sqlalchemy import String
+from sqlalchemy import ForeignKey, LargeBinary, Integer, String
 from .database import Base
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
 class User(Base):
