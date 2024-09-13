80b276fed1fa34340bba7a5ac8ddf5c41227ca5a
castromx <96194271+castromx@users.noreply.github.com>
2024-09-13 22:00:12 +0200

    Update task_router.py

--- click/task_router.py
+++ click/task_router.py
@@ -1,11 +1,7 @@
 from fastapi import APIRouter, BackgroundTasks
-from tasks import add_point_task, add_charge_count
+from tasks import add_charge_count
 router = APIRouter()
 
-@router.post('/add_celery_point/{user_id}')
-async def add_point_endpoint(user_id: int, count: int):
-    add_point_task.delay(user_id, count)
-    return {"message": "Add point tadsk has been added to the queue."}
 
 @router.post('/add_celery_charge/{user_id}')
 async def add_charge_count(user_id: int, count: int):
