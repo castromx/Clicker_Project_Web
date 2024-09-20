0f72e325475920021d7d0cedfa57656ac3cf9ed9
castromx <96194271+castromx@users.noreply.github.com>
2024-09-20 20:22:47 +0200

    Update Image.js

--- frontend__click/click_frontend/src/Pages/components/Image.js
+++ frontend__click/click_frontend/src/Pages/components/Image.js
@@ -2,47 +2,25 @@ import React, { useState } from 'react';
 
 const Image = ({ image, onClick }) => {
     const [isAnimating, setIsAnimating] = useState(false);
-    const [showPlusOne, setShowPlusOne] = useState(false);
 
     const handleClick = () => {
         setIsAnimating(true);
-        setShowPlusOne(true); 
         onClick();
     };
 
     const handleAnimationEnd = () => {
         setIsAnimating(false);
-        setTimeout(() => {
-            setShowPlusOne(false); // Сховати +1 через певний час
-        }, 1000); // Тривалість показу тексту +1
     };
 
     return (
-        <div className="image-container" style={{ position: 'relative' }}>
-            <img
-                className={`main-photo ${isAnimating ? 'animate' : ''}`}
-                src={image}
-                alt="User Icon"
-                onClick={handleClick}
-                onAnimationEnd={handleAnimationEnd}
-            />
-            {showPlusOne && (
-                <span className="plus-one" style={plusOneStyle}>
-                    +1
-                </span>
-            )}
-        </div>
+        <img
+            className={`main-photo ${isAnimating ? 'animate' : ''}`}
+            src={image}
+            alt="User Icon"
+            onClick={handleClick}
+            onAnimationEnd={handleAnimationEnd}
+        />
     );
-};
-
-const plusOneStyle = {
-    position: 'absolute',
-    top: '10px',
-    left: '10px',
-    fontSize: '24px',
-    color: 'red',
-    fontWeight: 'bold',
-    animation: 'fade-out 1s forwards',
 };
 
 export default Image;
