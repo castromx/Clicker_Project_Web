ea6d184531251322c1ef90c0cf4ea19895857497
castromx <96194271+castromx@users.noreply.github.com>
2024-10-04 22:20:00 +0200

    Delete tests directory

--- tests/test_main.py
+++
@@ -1,8 +0,0 @@
-from httpx import AsyncClient
-from .testconfig import ac
-
-
-async def test_first_point(ac: AsyncClient):
-    response = await ac.get("/")
-    assert response.status_code == 200
-
